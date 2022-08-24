73fe2344c9891dd94cadbddc0de00bd3006ebc14
Rainer Simon <aboutgeo@gmail.com>
2022-08-24 13:34:48 +0200

    Metadata conversion script for MAK data + compressed results

--- utils/mak-metadata/src/index.js
+++ utils/mak-metadata/src/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { XMLParser } from 'fast-xml-parser';
 import Papa from 'papaparse';
 
-const PATH_TO_MAK = '../data/MAK/Metadata/xml';
+const PATH_TO_MAK = '../../../data/MAK/Metadata/xml';
 
 const parser = new XMLParser({ ignoreAttributes: false });
 
@@ -21,14 +21,12 @@ const getReproductions = record => {
     .map(obj => obj['reproduction.reference'].replace('\\', '/'));
 }
 
-const getTextDE = obj => {
+const getLabelDE = obj => {
   if (obj?.value) {
     if (Array.isArray(obj.value)) {
       return obj.value.find(literal => literal['@_lang'] === 'de-DE' || literal['@_lang'] === '3')['#text'];
     } else if (obj?.value['@_lang'] === 'de-DE') {
       return obj.value['#text'];
-    } else {
-      return getFirstText(obj);
     }
   } else if (typeof obj === 'string' || obj instanceof String) {
     return obj;
@@ -42,25 +40,31 @@ function readOne(idx = 0) {
   const data = parser.parse(xml);
   const record = data.adlibXML.recordList.record;
 
-  const { priref, Title, Rights } = record;
+  const { priref, Description, Title, Rights } = record;
 
-  const title = Title && getTextDE(Title.title);
+  const title = getLabelDE(Title?.title);
+
+  const description = getLabelDE(Description?.description);
 
   const url = 'https://sammlung.mak.at/sammlung_online?id=collect-' + priref;
 
   const reproductions = getReproductions(record);
-
   const firstReproduction = reproductions.length > 0 ? reproductions[0] : null;
 
-  const rights = Rights && Rights['rights.consent_status'] && getTextDE(Rights['rights.consent_status']);
+  const rights = Rights && Rights['rights.consent_status'] && getLabelDE(Rights['rights.consent_status']);
 
-  meta.push({ 
-    priref, 
+  const compactRecord = {
+    priref,
     url, 
-    title, 
+    title,
+    description,
     reproduction: 'https://sammlung.mak.at/img/1200x1200/' + firstReproduction,
-    rights 
-  });
+    rights
+  }
+
+  // Only include records with "rights"="bewilligt"
+  if (rights === 'bewilligt')
+    meta.push(compactRecord);
 
   if (idx === files.length - 1) {
     console.log('Done - writing results');
