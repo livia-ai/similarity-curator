408205528e17e64fb1871561302a2cd08f13879b
Rainer Simon <aboutgeo@gmail.com>
2022-08-25 13:37:08 +0200

    Auto-download functionality for Qdrant init

--- server/api/src/Qdrant.js
+++ server/api/src/Qdrant.js
@@ -1,4 +1,9 @@
+import fs from 'fs';
+import fetch from 'node-fetch';
 import { Qdrant } from 'qdrant';
+
+const DATA_URL = 'https://dl.dropboxusercontent.com/s/uvz12ze382yuqyb/mak-vectors.jsonl.gz';
+const DATA_PATH = '../data/mak-vectors.jsonl.gz';
 
 const client = new Qdrant('http://localhost:6333/');
 
@@ -10,16 +15,39 @@ const SCHEMA = {
 const init = () => {
   console.log('[Qdrant] Checking if collections exists');
 
-  client.get_collection('livia').then(({ response }) => {
+  return client.get_collection('livia').then(({ response }) => {
     if (response.result) {
       console.log('[Qdrant] Exists');
     } else {
       console.log('[Qdrant] No collection - creating new');
-      client.create_collection('livia', SCHEMA).then(res => {
-        console.log('[Qdrant] Preparing data for ingest');
+      return client.create_collection('livia', SCHEMA).then(() => {    
+        if (!fs.existsSync(DATA_PATH))
+          return download().then(() => ingest());
+        else 
+          return ingest();
       });
     }
   })
+}
+
+const download = async () => {
+  console.log('[Qdrant] Downloading data - this may take a while');
+
+  const response = await fetch(DATA_URL);
+  const stream = fs.createWriteStream(DATA_PATH);
+
+  return new Promise((resolve, reject) => {
+    response.body.pipe(stream);
+    response.body.on('error', reject);
+    stream.on('finish', () => {
+      console.log('[Qdrant] Download complete');
+      resolve();
+    });
+  });
+}
+
+const ingest = async () => {
+  console.log('[Qdrant] Preparing data for ingest');
 }
 
 export default {
