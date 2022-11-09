70a61134be85422d3e764a0fb5a761da81cbaa94
Rainer Simon <aboutgeo@gmail.com>
2022-11-09 09:37:29 +0100

    Docker deployment fixes

--- server/src/Config.js
+++ server/src/Config.js
@@ -2,9 +2,9 @@ export default {
   API_PORT: 3000,
 
   DATA: [
-    ['WM',  '../data/combined_img2vec_sbert_wm_192d.jsonl.gz'],
-    ['MAK', '../data/combined_img2vec_sbert_mak_192d.jsonl.gz'],
-    ['BEL', '../data/combined_img2vec_sbert_bel_192d.jsonl.gz']
+    ['WM',  'data/combined_img2vec_sbert_wm_192d.jsonl.gz'],
+    ['MAK', 'data/combined_img2vec_sbert_mak_192d.jsonl.gz'],
+    ['BEL', 'data/combined_img2vec_sbert_bel_192d.jsonl.gz']
   ],
 
   DIMENSION: 192,
