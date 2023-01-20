f592ea3c46153f57f0c50e5391e46d6da7aae5d1
Rainer Simon <aboutgeo@gmail.com>
2023-01-20 13:54:58 +0100

    Added styling for SSR exhibit page + integrated Svelte island

---
+++ client/src/components/exhibit/ExhibitImage.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let src;
+
+  export let alt;
+</script>
+
+<img src={src} alt={alt} />
+
+<style> 
+  img {
+    width: 160px;
+    height: 160px;
+    object-fit: cover;
+    border-radius: 2px;
+  }
+</style>
