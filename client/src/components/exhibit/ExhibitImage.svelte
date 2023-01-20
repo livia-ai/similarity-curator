bdb5811c8fa5099bc57880a7e168bc7a85ab1331
Rainer Simon <aboutgeo@gmail.com>
2023-01-20 14:40:14 +0100

    Exhibit view: more Svelte plumbing

--- client/src/components/exhibit/ExhibitImage.svelte
+++ client/src/components/exhibit/ExhibitImage.svelte
@@ -1,16 +1,72 @@
 <script>
-  export let src;
+  import Icon from 'svelte-icons-pack/Icon.svelte';
+  import AiOutlineSearch from 'svelte-icons-pack/ai/AiOutlineSearch';
+  import ReadOnlyDetails from './ReadOnlyDetails.svelte';
 
-  export let alt;
+  export let item;
+
+  $: src = item.image_url;
+
+  $: alt = item.title || `Thumbnail ${item.id}`;
+
+  let showDetails = false;
 </script>
 
-<img src={src} alt={alt} />
+<div class="image-wrapper">
+  <div class="show-details">
+    <button on:click={() => showDetails = true}>
+      <Icon src={AiOutlineSearch} />
+    </button>
+  </div>
+
+  <img src={src} alt={alt} />
+</div>
+
+{#if showDetails}
+  <ReadOnlyDetails item={item} />
+{/if}
 
 <style> 
+  .image-wrapper {
+    position: relative;
+    border-radius: 5px;
+  }
+
+  .show-details {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    display: none;
+    border-radius: 5px;
+  }
+
+  .image-wrapper:hover .show-details {
+    display: block;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0.45);
+    fill: #fff;
+    font-size: 50px;
+    border-radius: 5px;
+  }
+
+  .show-details button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    opacity: 0.65;
+  }
+
   img {
     width: 160px;
     height: 160px;
     object-fit: cover;
-    border-radius: 2px;
+    border-radius: 5px;
   }
 </style>
